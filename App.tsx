d461da9f436ce34b4dca8e2492d76c166515b002
alexviar <alexanderviar@gmail.com>
2025-01-27 11:14:06 -0400

    fcm

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import BootSplash from "react-native-bootsplash";
 import SplashVideo from './SplashVideo';
 import MainScreen from './MainScreen';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, PermissionsAndroid, Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
   const [mainScreenReady, setMainScreenReady] = useState(false);
@@ -10,7 +11,34 @@ const App = () => {
     BootSplash.hide({ fade: true })
   }, [])
 
-  console.log(mainScreenReady)
+  const [
+    postNotificationsPermissionStatus,
+    setPostNotificationsPermissionStatus
+  ] = useState<string | null>(null)
+  useEffect(() => {
+    messaging()
+      .getToken()
+      .then((token) => {
+        console.log('ReactNativeLog', 'FCM Token:', token);
+      });
+    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS)
+      .then((value) => {
+        console.log('PermissionStatus', value)
+        setPostNotificationsPermissionStatus(value)
+      });
+  }, [])
+
+  useEffect(() => {
+    // if (postNotificationsPermissionStatus !== 'granted') return
+
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('ReactNativeLog', JSON.stringify(remoteMessage))
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, [postNotificationsPermissionStatus]);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       {!mainScreenReady && (
