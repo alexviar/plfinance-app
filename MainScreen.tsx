87bef00a7898ab740868c12eaa9e90f8935da710
alexviar <alexanderviar@gmail.com>
2025-01-31 15:51:24 -0400

    feat: isLocked

--- MainScreen.tsx
+++ MainScreen.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { StyleSheet, Animated } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+import React, { useEffect, useRef } from 'react';
+import { StyleSheet, Animated, NativeModules, Alert } from 'react-native';
 import WebView from 'react-native-webview';
 
 type Props = {
@@ -7,25 +8,46 @@ type Props = {
 }
 
 const MainScreen = ({ onReady }: Props) => {
-  // const opacity = new Animated.Value(0);
+  const webViewRef = useRef<WebView>(null);
   const [visible, setVisible] = React.useState(false)
 
+  useEffect(() => {
+    const isLocked: boolean = NativeModules.DeviceManagement.isLocked();
+    webViewRef.current?.injectJavaScript(`localStorage.setItem('locked', String(${isLocked ? 'true' : 'false'}))`)
+  }, [])
+
   return (
-    // <Animated.View style={[styles.container, { opacity }]}>
     <WebView
+      ref={webViewRef}
       style={{ display: visible ? 'flex' : 'none' }}
       source={{ uri: "http://plfinancedev.eastus.cloudapp.azure.com" }}
+      onMessage={async ({ nativeEvent: { data } }) => {
+        try {
+          const { event, payload } = JSON.parse(data);
+          if (event === 'installments_received') {
+            console.log("Hola")
+            const pendingToken = messaging()
+              .getToken()
+            console.log("Mundo")
+            payload.forEach((installment: { id: number, dueDate: string }) => {
+              NativeModules.DeviceManagement.scheduleDeviceLock(installment.id, Date.parse(installment.dueDate));
+            })
+            let token
+            try {
+              token = await pendingToken
+              console.log('ReactNativeLog', 'FCM Token:', token);
+            } catch { }
+            webViewRef.current?.injectJavaScript(`registerDevice(${payload.purchaseId}, ${token ?? 'null'})`)
+          }
+        } catch (error) {
+          Alert.alert('Error al procesar el mensaje:' + error);
+        }
+      }}
       onLoadEnd={() => {
         onReady?.()
         setVisible(true)
-        // Animated.timing(opacity, {
-        //   toValue: 1,
-        //   duration: 300,
-        //   useNativeDriver: true,
-        // }).start();
       }}
     />
-    // </Animated.View>
   );
 };
 
