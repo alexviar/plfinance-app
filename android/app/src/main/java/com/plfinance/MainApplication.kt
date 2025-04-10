21eee9e141f96605248effe236011834907424e7
alexviar <alexanderviar@gmail.com>
2025-04-10 01:51:47 -0400

    fix: add UpdaterPackage to the list of React packages

--- android/app/src/main/java/com/plfinance/MainApplication.kt
+++ android/app/src/main/java/com/plfinance/MainApplication.kt
@@ -21,6 +21,7 @@ class MainApplication : Application(), ReactApplication {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
               add(com.plfinance.DeviceManagementPackage())
+              add(com.plfinance.updater.UpdaterPackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
